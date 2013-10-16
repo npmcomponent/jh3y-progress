d6435ce8e6bbd0ced70a13aef59f49433dcc6142
jheytompkins <jhey.tompkins@gmail.com>
2013-10-16 17:18:55 +0100

    Styled progress and added two functions for setting color and progress percentage.

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 module.exports = progress;
-function progress() {
+function progress(color) {
 	if (!(this instanceof progress)) return new progress();
-	this.element = element;
+	this._color = (color !== undefined) ? color: '#00FF00';
+	this._progress = 0;
 	this._create();
 }
 progress.prototype._create = function () {
 	var progress = this;
+	progress.element = document.createElement('div');
+	progress.element.className = 'progress';
+	var progressIndicator = document.createElement('div');
+	progressIndicator.className = 'progress-indicator';
+	progressIndicator.style.background = progress._color;
+	progress.element.appendChild(progressIndicator);
+}
+progress.prototype.setProgress = function (progress) {
+	if (progress !== undefined && typeof(progress) === 'number' && progress <= 100 && progress >= 0) {
+		this.element.querySelector('.progress-indicator').style.width = progress + '%';	
+	}
+}
+progress.prototype.setColor = function (color) {
+	if (color !== undefined && typeof(color) === 'string') {
+		this.element.querySelector('.progress-indicator').style.background = color;	
+	}	
 }
