8dc27468f26bf3108cfc4e0aed3b3d97b53dd2cd
jheytompkins <jhey.tompkins@gmail.com>
2013-10-25 16:24:12 +0100

    Sorted out readme and added error throw on erroneous values.

--- index.js
+++ index.js
@@ -18,11 +18,15 @@ progress.prototype.setProgress = function (progress) {
 	if (progress !== undefined && typeof(progress) === 'number' && progress <= 100 && progress >= 0) {
 		this.element.querySelector('.progress-indicator').style.width = progress + '%';
 		this._progress = progress;	
+	} else {
+		throw Error('progress: ERROR progress must be a number and between 0 and 100.')
 	}
 }
 progress.prototype.setColor = function (color) {
 	if (color !== undefined && typeof(color) === 'string') {
 		this.element.querySelector('.progress-indicator').style.background = color;	
 		this._color = color;
-	}	
+	} else {
+		throw Error('progress: ERROR progress must be a string.')
+	}
 }
--- progress.js
+++ progress.js
@@ -221,14 +221,19 @@ progress.prototype.setProgress = function (progress) {
 	if (progress !== undefined && typeof(progress) === 'number' && progress <= 100 && progress >= 0) {
 		this.element.querySelector('.progress-indicator').style.width = progress + '%';
 		this._progress = progress;	
+	} else {
+		throw Error('progress: ERROR progress must be a number and between 0 and 100.')
 	}
 }
 progress.prototype.setColor = function (color) {
 	if (color !== undefined && typeof(color) === 'string') {
 		this.element.querySelector('.progress-indicator').style.background = color;	
 		this._color = color;
-	}	
+	} else {
+		throw Error('progress: ERROR progress must be a string.')
+	}
 }
+
 });
 require.alias("progress/index.js", "progress/index.js");if (typeof exports == "object") {
   module.exports = require("progress");
